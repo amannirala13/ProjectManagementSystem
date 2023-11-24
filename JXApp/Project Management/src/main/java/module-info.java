be7c134e6071daa0d631f2a862ae64775c4c4e3b
amannirala13 <asdevofficial@gmail.com>
2023-11-24 21:22:55 +0530

    Init JavaFx Project

---
+++ JXApp/Project Management/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.amannirala13.projectmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.amannirala13.projectmanagement to javafx.fxml;
+    exports com.amannirala13.projectmanagement;
+}
