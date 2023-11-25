992d645911ceb205527fb61bb154e803e28a9d90
Shashank Kothari <marutikothari39@gmail.com>
2023-11-25 15:28:37 +0530

    Update ProjectPageController.java

--- JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/ProjectPageController.java
+++ JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/ProjectPageController.java
@@ -1,5 +1,42 @@
 package com.amannirala13.projectmanagement.controllers;
 
+import com.amannirala13.projectmanagement.database.DBConnector;
+import com.amannirala13.projectmanagement.database.project.DBProject;
+import com.amannirala13.projectmanagement.helper.Helper;
+import com.amannirala13.projectmanagement.navigation.Routes;
+import com.amannirala13.projectmanagement.navigation.SceneNavigation;
+import java.sql.ResultSet;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ProjectPageController {
 
+    
+
+
+
+    @FXML
+    public void showList() {
+        ResultSet resultSet = DBConnector.getInstance().runSelectQuery("SELECT * FROM tasks;");
+        
+    }
+
+    @FXML
+    public void addTask(){
+    }
+    
+    @FXML
+    public void delete(){
+    }
+
+    @FXML
+    public void back(){
+        SceneNavigation.getInstance(null).activateScreen(Routes.DASHBOARD);
+    }
+    
+    @FXML
+    public void home(){
+        SceneNavigation.getInstance(null).activateScreen(Routes.DASHBOARD);
+    }
 }
+
