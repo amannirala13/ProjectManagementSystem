53d0541e85ac490dbd98468d3b359ba35ea893f3
Shashank Kothari <marutikothari39@gmail.com>
2023-11-25 14:25:24 +0530

    Create TaskController.java

---
+++ JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/TaskController.java
@@ -0,0 +1,63 @@
+package com.amannirala13.projectmanagement.controllers;
+
+import com.amannirala13.projectmanagement.database.project.DBProject;
+import com.amannirala13.projectmanagement.helper.Helper;
+import com.amannirala13.projectmanagement.navigation.Routes;
+import com.amannirala13.projectmanagement.navigation.SceneNavigation;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreateProjectController {
+
+    @FXML
+    private TextField projectIdText;
+    @FXML
+    private TextField projectNameText;
+    @FXML
+    private TextField projectStartDate;
+    @FXML
+    private TextField projectEndDate;
+    @FXML
+    private TextField projectBufferDaysText;
+
+
+
+    @FXML
+    public void reset() {
+        projectIdText.setText("");
+        projectNameText.setText("");
+        projectStartDate.setText("");
+        projectEndDate.setText("");
+        projectBufferDaysText.setText("");
+    }
+
+    @FXML
+    public void submit(){
+        String projectName = projectNameText.getText();
+        String startDate = projectStartDate.getText();
+        String endDate = projectEndDate.getText();
+        String bufferDays = projectBufferDaysText.getText();
+
+        boolean status = DBProject.createProject(
+                projectName,
+                startDate,
+                endDate,
+                bufferDays,
+                false
+        );
+
+        if(!status) {
+            System.out.println("Unable to create project!!");
+        }
+        else {
+            System.out.println("Created project");
+            reset();
+        }
+    }
+
+    @FXML
+    public void back(){
+        SceneNavigation.getInstance(null).activateScreen(Routes.DASHBOARD);
+    }
+}
+
