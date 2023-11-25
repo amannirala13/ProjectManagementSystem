0e440288759adaca122961064586e456c2460c45
KaivalyaVIT <kaivalya.sao23@vitstudent.ac.in>
2023-11-25 15:32:45 +0530

    Changes made

--- JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/ProjectPageController.java
+++ JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/ProjectPageController.java
@@ -2,6 +2,12 @@ package com.amannirala13.projectmanagement.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import com.amannirala13.projectmanagement.database.DBConnector;
+import com.amannirala13.projectmanagement.database.project.DBProject;
+import com.amannirala13.projectmanagement.helper.Helper;
+import com.amannirala13.projectmanagement.navigation.Routes;
+import com.amannirala13.projectmanagement.navigation.SceneNavigation;
+import java.sql.ResultSet;
 
 public class ProjectPageController {
     @FXML
@@ -18,4 +24,32 @@ public class ProjectPageController {
     @FXML
     private Label remainingDays;
 
+    
+
+
+
+    @FXML
+    public void showList() {
+        ResultSet resultSet = DBConnector.getInstance().runSelectQuery("SELECT * FROM projects;");
+        
+    }
+
+    @FXML
+    public void addTask(){
+    }
+    
+    @FXML
+    public void delete(){
+    }
+
+    @FXML
+    public void back(){
+        SceneNavigation.getInstance(null).activateScreen(Routes.DASHBOARD);
+    }
+    
+    @FXML
+    public void home(){
+        SceneNavigation.getInstance(null).activateScreen(Routes.DASHBOARD);
+    }
 }
+
