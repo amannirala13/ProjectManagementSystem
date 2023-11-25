07a924ce204d08a1e2f7c030f0d9e016de4ee2ef
KaivalyaVIT <kaivalya.sao23@vitstudent.ac.in>
2023-11-25 16:23:03 +0530

    Changes made

--- JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/CurrentProjectController.java
+++ JXApp/Project Management/src/main/java/com/amannirala13/projectmanagement/controllers/CurrentProjectController.java
@@ -3,10 +3,20 @@ package com.amannirala13.projectmanagement.controllers;
 import com.amannirala13.projectmanagement.navigation.Routes;
 import com.amannirala13.projectmanagement.navigation.SceneNavigation;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class CurrentProjectController {
     @FXML
     public void back(){
         SceneNavigation.getInstance(null).activateScreen(Routes.DASHBOARD);
     }
+
+    @FXML
+    private Label startDate;
+    @FXML
+    private Label endDate;
+    @FXML
+    private Label bufferDays;
+    @FXML
+    private Label projStatus;
 }
